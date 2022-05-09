9893a289e2e80a9cffd6bec132c66bf87139c7a9
aurel1403 <aurelie.boribon@gmail.com>
2022-05-09 22:00:14 +0400

    Update signin.php

--- signin.php
+++ signin.php
@@ -2,9 +2,9 @@
 	
 session_start();
 
-$conn = mysqli_connect('localhost','root','office');
+$conn = mysqli_connect('localhost','root','123456');
 
-mysqli_select_db($conn, 'Registration');
+mysqli_select_db($conn, 'office');
 
 $sname = $_POST['sname'];
 $fname = $_POST['fname'];
@@ -12,7 +12,7 @@ $user = $_POST['user'];
 $password = $_POST['password'];
 $num = $_POST['num'];
 
-$s = "select * from usertable where user = '$user'";
+$s = "select * from Registration where user = '$user'";
 
 $result = mysqli_query($conn, $s);
 
@@ -21,7 +21,7 @@ $number = mysqli_number_rows($result);
 if($number == 1){
     echo" Username Already Taken";
 } else{
-    $reg= " insert into usertable(sname, fname, user, password, num) values ('$sname','$fname','$user','$password','$num')";
+    $reg= " insert into Registration(sname, fname, user, password, num) values ('$sname','$fname','$user','$password','$num')";
     mysqli_query($conn, $reg);
     echo" Registration Successful";
 
