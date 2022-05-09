19cb0237e73c22a3246caddfec7308d980b28d9d
aurel1403 <aurelie.boribon@gmail.com>
2022-05-09 21:55:40 +0400

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,22 +1,22 @@
 <?php
-	$sname = $_POST['sname'];
-	$fname = $_POST['fname'];
-	$user = $_POST['user'];
-	$password = $_POST['password'];
-	$num = $_POST['num'];
+    $sname = $_POST['sname'];
+    $fname = $_POST['fname'];
+    $user = $_POST['user'];
+    $password = $_POST['password'];
+    $num = $_POST['num'];
 
-	// Database connection
-	$conn = new mysqli('localhost','root','','office');
-	if($conn->connect_error){
-		echo "$conn->connect_error";
-		die("Connection Failed : ". $conn->connect_error);
-	} else {
-		$stmt = $conn->prepare("insert into Registration(sname, fname, user, password, num) values(?, ?, ?, ?, ?)");
-		$stmt->bind_param("ssssi", $sname, $fname, $user, $password, $num);
-		$execval = $stmt->execute();
-		echo $execval;
-		echo "Registration successful...";
-		$stmt->close();
-		$conn->close();
-	}
+    // Database connection
+    $conn = new mysqli('localhost','root','','office');
+    if($conn->connect_error){
+        echo "$conn->connect_error";
+        die("Connection Failed : ". $conn->connect_error);
+    } else {
+        $stmt = $conn->prepare("insert into Registration(sname, fname, user, password, num) values(?, ?, ?, ?, ?)");
+        $stmt->bind_param("ssssi", $sname, $fname, $user, $password, $num);
+        $execval = $stmt->execute();
+        echo $execval;
+        echo "Registration successful...";
+        $stmt->close();
+        $conn->close();
+    }
 ?>
